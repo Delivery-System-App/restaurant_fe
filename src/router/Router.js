7550246ea35cc1091fc683d8c7a524a15cd50490
Jishnu P Sreekumar <psjishnu13@gmail.com>
2020-08-24 19:17:25 +0530

    changes filename

--- src/router/Router.js
+++ src/router/Router.js
@@ -3,7 +3,7 @@ import PublicRouter from "./PublicRouter";
 import AuthenticatedRouter from "./AuthenticatedRouter";
 import { useAbortableEffect } from "../utils/UseAbortableEffect";
 import { useDispatch, useSelector } from "react-redux";
-import { getCurrentUser } from "../redux/apiActions";
+import { getCurrentUser } from "../redux/apiAction";
 import { navigate } from "hookrouter";
 import Loader from "../utils/Loader";
 const Router = () => {
