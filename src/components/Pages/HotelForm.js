8fadc75623623662e89db6bde23024e760650729
Jishnu P Sreekumar <psjishnu13@gmail.com>
2020-08-28 00:30:32 +0530

    Added loader while submitting

--- src/components/Pages/HotelForm.js
+++ src/components/Pages/HotelForm.js
@@ -6,6 +6,8 @@ import { Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import { Card } from "@material-ui/core";
 import Uploader from "./UploadImage";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { green } from "@material-ui/core/colors";
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -13,10 +15,62 @@ const useStyles = makeStyles((theme) => ({
     margin: "0px auto",
     padding: "20px 30px 20px 30px",
   },
+
   submit: {
-    margin: theme.spacing(3, 0, 2),
+    //margin: theme.spacing(3, 0, 2),
+  },
+  wrapper: {
+    // margin: theme.spacing(1),
+    position: "relative",
+  },
+
+  buttonProgress: {
+    color: green[500],
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    marginTop: -12,
+    marginLeft: -12,
   },
 }));
+
+const LoaderButton = ({ Loading, handleSubmit, type }) => {
+  const classes = useStyles();
+  const [loading, setLoading] = React.useState(false);
+
+  React.useEffect(() => {
+    let mount = true;
+    if (mount) {
+      if (!Loading) {
+        setLoading(false);
+      } else {
+        setLoading(true);
+      }
+    }
+    return () => {
+      mount = false;
+    };
+  }, [Loading]);
+
+  return (
+    <div className={classes.wrapper}>
+      <Button
+        variant="contained"
+        color="primary"
+        disabled={loading}
+        onClick={handleSubmit}
+        fullwidth
+        className={classes.submit}
+      >
+        {type} Hotel
+      </Button>
+      {loading && (
+        <CircularProgress size={24} className={classes.buttonProgress} />
+      )}
+    </div>
+  );
+};
+
 const HotelForm = ({
   handleChange,
   handleSubmit,
@@ -25,6 +79,7 @@ const HotelForm = ({
   Error,
   type,
   Helper,
+  Loading,
 }) => {
   const classes = useStyles();
 
@@ -78,7 +133,11 @@ const HotelForm = ({
             />
           </Grid>
           <Grid item xs={12}>
-            <Uploader setFiles={setFiles} formLoading={false} />
+            <Uploader
+              setFiles={setFiles}
+              clear={Form.photos}
+              formLoading={false}
+            />
             {Helper}
           </Grid>
           <Grid item xs={12}>
@@ -96,15 +155,13 @@ const HotelForm = ({
             />
           </Grid>
           <Grid item xs={12}>
-            <Button
-              onClick={handleSubmit}
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-            >
-              {type} Hotel
-            </Button>
+            <div className="text-center">
+              <LoaderButton
+                type={type}
+                handleSubmit={handleSubmit}
+                Loading={Loading}
+              />
+            </div>
           </Grid>
         </Grid>
       </form>
