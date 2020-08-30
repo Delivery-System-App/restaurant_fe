e3c476c27c9da7230abcaa5fbd2079c496cd09a6
Jishnu P Sreekumar <psjishnu13@gmail.com>
2020-08-30 17:44:21 +0530

    updated search width

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,19 +9,21 @@ import SearchIcon from "@material-ui/icons/Search";
 
 const SearchBar = ({ searchChange }) => {
   return (
-    <div className="w-full my-2 text-center">
-      <FormControl>
-        <InputLabel htmlFor="input">Search</InputLabel>
-        <Input
-          id="input"
-          onChange={searchChange}
-          startAdornment={
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          }
-        />
-      </FormControl>
+    <div className="w-full">
+      <div className="my-2 w-3/4 md:w-1/3 lg:w-1/3 m-0 m-auto text-center">
+        <FormControl style={{ width: "100%" }}>
+          <InputLabel htmlFor="input">Search</InputLabel>
+          <Input
+            id="input"
+            onChange={searchChange}
+            startAdornment={
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </div>
     </div>
   );
 };
