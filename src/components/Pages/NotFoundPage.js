ea5fa53d26b970f0836d45dcef00657bb88eeb8e
Jishnu P Sreekumar <psjishnu13@gmail.com>
2020-09-11 19:52:13 +0530

    updated 404 page

--- src/components/Pages/NotFoundPage.js
+++ src/components/Pages/NotFoundPage.js
@@ -4,10 +4,14 @@ import { Grid } from "@material-ui/core";
 
 const NotFoundPage = () => {
   useHeading("Oops! Not Found");
-  const image = require("../../assets/images/notfound.png");
+  const image = require("../../assets/images/notfound.svg");
   return (
-    <Grid container justify="center">
-      <img src={image} alt={"Page not found!!"} />
+    <Grid container className="px-3" justify="center">
+      <img
+        className="mt-40 md:mt-10 lg:mt-5"
+        src={image}
+        alt={"Page not found!!"}
+      />
     </Grid>
   );
 };
